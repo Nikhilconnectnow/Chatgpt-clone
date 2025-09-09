4f9df6042edbe46cf7e5d0cc3b63858e2433fe53
Nikhil <nikhil.connectnow@gmail.com>
2025-09-09 08:46:15 +0000

    adding auth

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cookieParser= require('cookie-parser');
 const app= express();
+const authRoute= require('./routes/auth.route')
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.use('/auth',authRoute);
 
 
 module.exports =app;
