9590b4e4d35959257e3359b391840498bdc6e440
Nikhil <nikhil.connectnow@gmail.com>
2025-09-09 04:19:30 +0000

    Apply .gitignore properly

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const app = require('./src/app');
+
+const connectDb=require('./src/db/db');
+
+
+connectDb();
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is working on ${process.env.PORT}`)
+});
+
+
+
